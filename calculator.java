9cfafe3626a3fa0f9b141dc5c9f194db8a323daf
Ammar <ammarahmedkhan29@gmail.com>
2019-06-02 22:29:52 -0400

    updated calc

--- calculator.java
+++ calculator.java
@@ -13,22 +13,24 @@ public class calculator {
     Scanner opin = new Scanner(System.in);
     System.out.println("What operation would you like to do? [1] = +, [2]= -, [3] = *, [4] = /, [n] = exit");
     String originalOp = opin.nextLine();
-    case 1:
+    switch(originalOp){
+    case "1":
       System.out.println("What numbers do you want to add? (# #)");
       Scanner addIn1 = new Scanner(System.in);
       int A1 = addIn1.nextInt();
       Scanner addIn2 = new Scanner(System.in);
       int A2 = addIn2.nextInt();
       System.out.println(addition(A1, A2));
-    case 2:
+      break;
+    case "2":
       System.out.println("What numbers do you want to subtract?");
-      Scanner addIn1 = new Scanner(System.in);
-      int A1 = addIn1.nextInt();
-      Scanner addIn2 = new Scanner(System.in);
-      int A2 = addIn2.nextInt();
-      System.out.println(addition(A1, A2));
-    case 3:
-    case 4:
+      Scanner subIn1 = new Scanner(System.in);
+      int S1 = subIn1.nextInt();
+      Scanner subIn2 = new Scanner(System.in);
+      int S2 = subIn2.nextInt();
+      System.out.println(subtraction(S1, S2));
+    case "3":
+    case "4":
+      }
     }
   }
-}
